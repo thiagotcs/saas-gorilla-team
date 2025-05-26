a146a9866fab3abcd1ad1582b4c1dc4a5ce9614d
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-05-26 08:44:08 -0300

    feat(api): implement ability definitions for user role management

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { defineAbilityFor, roleSchema } from '@saas/auth'
+
+const ability = defineAbilityFor({
+  role: roleSchema.parse('INSTRUTOR'),
+  id: 'user-id',
+  __typename: 'User',
+})
+console.log(ability.can('get', 'User'))
+console.log(ability.can('create', 'User'))
