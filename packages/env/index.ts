8b3b9e1c5c601002f8468344d7473ce244c7c356
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-06-15 20:33:18 -0300

    feat(env): add environment configuration with server and runtime settings

---
+++ packages/env/index.ts
@@ -0,0 +1,25 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    SERVER_PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().url(),
+    JWT_SECRET: z.string(),
+
+    GOOGLE_OAUTH_CLIENT_ID: z.string(),
+    GOOGLE_OAUTH_CLIENT_SECRET: z.string(),
+    GOOGLE_OAUTH_REDIRECT_URI: z.string().url(),
+  },
+  client: {},
+  shared: {},
+  runtimeEnv: {
+    SERVER_PORT: process.env.SERVER_PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_CLIENT_REDIRECT_URI,
+  },
+  emptyStringAsUndefined: true,
+})
