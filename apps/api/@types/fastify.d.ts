8cc412a3f54b98064dd0439c4f2d7a98937c59cb
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-06-20 08:23:21 -0300

    feat(types): add getUserMembership method to FastifyRequest interface

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import type { Member, Academy } from '@prisma/client'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string
+    ): Promise<{ academy: Academy; membership: Member }>
   }
 }
