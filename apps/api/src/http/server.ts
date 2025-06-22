51aa6f6c5e2bf573280b86cd6015ec5b21d376b8
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-06-22 12:03:59 -0300

    feat(routes): add training group and member management routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,22 @@ import { getAcademy } from '@/http/routes/gyms/get-academy'
 import { updateAcademy } from '@/http/routes/gyms/update-academy'
 import { shutdownAcademy } from '@/http/routes/gyms/shutdown-academy'
 import { transferAcademy } from '@/http/routes/gyms/transfer-academy'
+import { createTrainingGroup } from '@/http/routes/training-groups/create-training-group'
+import { deleteTrainingGroup } from '@/http/routes/training-groups/delete-training-group'
+import { getTrainingGroup } from '@/http/routes/training-groups/get-training-group'
+import { getTrainingGroups } from '@/http/routes/training-groups/get-training-groups'
+import { updateTrainingGroup } from '@/http/routes/training-groups/update-training-group'
+import { getMembers } from '@/http/routes/members/get-members'
+import { updateMember } from '@/http/routes/members/update-member'
+import { removeMember } from '@/http/routes/members/remove-member'
+import { createInvite } from '@/http/routes/invites/create-invite'
+import { getInvite } from '@/http/routes/invites/get-invite'
+import { getInvites } from '@/http/routes/invites/get-invites'
+import { acceptInvite } from '@/http/routes/invites/accept-invite'
+import { rejectInvite } from '@/http/routes/invites/reject-invite'
+import { revokeInvite } from '@/http/routes/invites/revoke-invite'
+import { getPendingInvites } from '@/http/routes/invites/get-pending-invites'
+import { getAcademyBilling } from '@/http/routes/billing/get-academy-billing'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -69,13 +85,34 @@ app.register(authenticateWithGoogle)
 
 app.register(requestPasswordRecover)
 app.register(resetPassword)
-app.register(createAcademy)
 app.register(getMembership)
+
+app.register(createAcademy)
 app.register(getAcademys)
 app.register(getAcademy)
 app.register(updateAcademy)
 app.register(shutdownAcademy)
 app.register(transferAcademy)
+
+app.register(createTrainingGroup)
+app.register(deleteTrainingGroup)
+app.register(getTrainingGroup)
+app.register(getTrainingGroups)
+app.register(updateTrainingGroup)
+
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
+
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
+app.register(getPendingInvites)
+
+app.register(getAcademyBilling)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
