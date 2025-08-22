04cc9df46f4a47687da236c893e9ab585ba82669
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-08-22 18:38:49 -0300

    feat: update AppLayout to include sheet prop for rendering additional content

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -3,12 +3,19 @@ import { redirect } from 'next/navigation'
 import { isAuthenticated } from '@/lib/server-auth-utils'
 export default async function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!(await isAuthenticated())) {
     redirect('/auth/sign-in')
   }
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      {sheet}
+    </>
+  )
 }
