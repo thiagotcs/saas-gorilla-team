d87baf63b82ffcb6f964420740b03af22bde1d15
Thiago Carvalho <thiagotcs.dev@gmail.com>
2025-09-20 23:41:23 -0300

    feat: add fetchHygraphQuery utility for data fetching from Hygraph

---
+++ apps/web/src/utils/fetch-hygraph-query.ts
@@ -0,0 +1,27 @@
+export const fetchHygraphQuery = async <T>(
+  query: string,
+  revalidate?: number
+): Promise<T> => {
+  const response = await fetch(process.env.HYGRAPH_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+    },
+    next: {
+      revalidate,
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  })
+
+  if (!response.ok) {
+    throw new Error(`Erro na request Hygraph: ${response.statusText}`)
+  }
+
+  const { data } = await response.json()
+
+  return data
+}
